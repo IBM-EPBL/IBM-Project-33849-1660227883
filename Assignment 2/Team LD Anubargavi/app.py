3f6944f5bace91c526f2e7f1896703319e69e9c1
ANUBARGAVI <112374272+ANUBARGAVI@users.noreply.github.com>
2022-09-25 16:41:08 +0530

    Add files via upload

---
+++ Assignment 2/Team LD Anubargavi/app.py
@@ -0,0 +1,25 @@
+
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/registration')
+def Registration():
+    return render_template('Registration.html')
+@app.route('/home')
+def Home():
+    return render_template('Home.html')
+
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
+
+
